a094499e45fa7dd39d483c4451f112375a0f5f77
AndreGallini <andrea.gallini01@libero.it>
2022-11-29 16:03:17 +0100

    add file

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+GeneraPassword();
+function GeneraPassword()
+{
+    $alfabetoMinuscolo = "abcdefghilmnopqestuvz";
+    $alfabetoMaiuscolo = strtoupper($alfabetoMinuscolo);
+    $numeri = 1234567890;
+    $simboli = "!£$%&/()=?^";
+    $lunghezzaPsw = $_GET["password"];
+    echo $lunghezzaPsw;
+
+    for ($i = 0; $i < $lunghezzaPsw; $i++) {
+        echo substr($alfabetoMinuscolo, 0, $i);
+
+    }
+}
+;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form action="index.php" method="get">
+
+            <input type="number" id="psw" name="password">
+            <button type="submit">Genera la tua password</button>
+        </form>
+    </div>
+</body>
+
+</html>
